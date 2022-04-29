e7dc904f821c9f2f0e4a9c863a4192d7c8cc5e6d
akmahamud <am3153636@gmail.com>
2022-04-29 15:16:26 +0600

    some add style

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./components/Login/Login";
 import Navbar from "./components/Navbar/Navbar";
 import Register from "./components/Register/Register";
 
+
 function App() {
   return (
     <>
