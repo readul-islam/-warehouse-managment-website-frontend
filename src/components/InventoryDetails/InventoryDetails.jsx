4aca27fdc4af9722fc9e836010573f365aaef2a6
akmahamud <am3153636@gmail.com>
2022-05-07 18:01:47 +0600

    update quantity complete

--- src/components/InventoryDetails/InventoryDetails.jsx
+++ src/components/InventoryDetails/InventoryDetails.jsx
@@ -8,11 +8,14 @@ import { Link, useParams } from "react-router-dom";
 
 
 const InventoryDetails = () => {
+  const { id } = useParams();
   const init = 0;
   const [selected , setSelected] = useState({})
-  const [quan , setQuan] = useState(0)
-  const [isLoading , setIsLoading] = useState(false)
-  const { id } = useParams();
+  const [quantity, setQuantity] = useState(false)
+ 
+  
+
+
   
  
   useEffect(() => {
@@ -22,45 +25,51 @@ const InventoryDetails = () => {
     .then(data => {
       setSelected(data)
     })
-},[id,isLoading])
+},[id,quantity])
   
 const updateQuan = (add, deleted) => {
+  
+
     fetch(`http://localhost:5000/add-inventory/${id}?add=${add}&deleted=${deleted}`,{
       method: 'PUT',
      
     })
     .then(res => res.json())
     .then(data =>{
+      
       if(data.error){
 
         return toast.error(data.error,{id:1})
       }
+      setQuantity(!quantity)
     })
+    
+    
   }
-  const quantity =(event) =>{
-    const quan =  event.target.value;
-    if(quan){
-      setQuan(quan )
-     
-    }
-   
-   
-  }
+  // const inputHandler =(event) =>{
+  //   const inputQuan =  event.target.value;
+  //   if(inputQuan){
+  //     setQuan(inputQuan )
+  //     setQuantity(inputQuan)
+  //   }
+  // }
 
 //conceptul session asking
-  const addQuantity = ()=>{
-
-
-    updateQuan(quan,init)
-    setIsLoading(!isLoading)
-  
-  }
+  const addQuantity = (event)=>{
+    event.preventDefault();
+    const addValue = event.target.quan.value
+    if(addValue){
+     console.log(addValue)
+ updateQuan(addValue,init)
+   }
+   
+   }
   
   
   const deleteQuantity = ()=>{
-    updateQuan(init,1)
-    setIsLoading(!isLoading)
-  }
+    updateQuan(init, 1)
+   
+   }
   
 
   return (
@@ -68,21 +77,21 @@ const updateQuan = (add, deleted) => {
    <div className="w-screen ">
       <div className="w-full grid grid-cols-4 lg:px-24 ">
         <div>
-          <div className="flex flex-col font-semibold  mt-8">
+          <form  onSubmit={addQuantity} className="flex flex-col font-semibold  mt-8">
             <div
              
               className="flex items-center "
             > 
-          <input onBlur={quantity}
+          <input 
                   className="border rounded px-3 py-1 mt-2 focus:outline-0"
-                  type="text" name="quantity"
-                  placeholder="add" required
+                  type="text" name="quan"
+                  placeholder="add" 
                 />
             
              
             </div>
             <div  className="flex items-center space-x-2 mt-7">
-            <button onClick={addQuantity}
+            <button
               type="submit"
               className="text-white  bg-blue-700 hover:bg-blue-800  font-medium rounded-full text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
@@ -91,14 +100,14 @@ const updateQuan = (add, deleted) => {
             
             <button
             onClick={deleteQuantity}
-              type="submit"
+              
               className="text-white  bg-blue-700 hover:bg-blue-800  font-medium rounded-full text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
              Delivered
             </button>
            
             </div>
-          </div>
+          </form>
         </div>
         <div className="  col-span-3 bg-[#F4F7FC]">
         <div className=" px-8  mt-8 w-full h-[87vh] ">
@@ -110,7 +119,7 @@ const updateQuan = (add, deleted) => {
               <div className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{selected?.title}</div>
               <div className="italic">suplierName:</div>
               <div  className='font-semibold text-sm mt-'>Id:{selected._id}</div>
-              <div>quantity: {selected?.quantity}</div>
+              <div>quantity: {selected.quantity}</div>
               <div className="mb-3 font-normal text-gray-700 dark:text-gray-400">description: {selected?.description}</div>
               <div className='font-semibold'>
             Price: $
