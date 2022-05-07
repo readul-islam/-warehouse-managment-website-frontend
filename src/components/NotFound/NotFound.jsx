2882e09059e58e8e4b8509f316262cad58aca3d6
akmahamud <am3153636@gmail.com>
2022-05-08 02:13:36 +0600

    update nofound page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div className='flex justify-center mt-4 md:mt-8 lg:mt-14'>
-            <img className='w-[600px] h-[500px]' src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKeiojHtqSReVaxvF870o1YK1YXHPcTgUw-AyoLyEhIWQrIDwoPNxp_6_NMVGewa_IVtE&usqp=CAU`} alt="" />
+            <img className='md:w-[50%] w-[90%]' src={`https://i.ibb.co/d2tpgQ7/404-error-page-not-found.png`} alt="" />
         </div>
     );
 };
