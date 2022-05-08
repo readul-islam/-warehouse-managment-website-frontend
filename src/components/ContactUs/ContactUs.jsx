3892e9380203b1b87f92dfd9a2d9fcab0baa6257
akmahamud <am3153636@gmail.com>
2022-05-08 22:56:23 +0600

    update contact us

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 const ContactUs = () => {
     return (
-        <div>
-            <h1></h1>
+       <div className="w-full  flex justify-center">
+            <div className="pt-6 text-lg  ">
+            <h1>Email: Example@gmail.com</h1>
+            <h1>Phone No: +8801701010101</h1>
+            <h1>Address: Aliabat, Katrina city, India</h1>
         </div>
+       </div>
     );
 };
 
