55f3189dc9e09e12aa434d5aa025a30923543b1e
akmahamud <am3153636@gmail.com>
2022-05-08 09:36:30 +0600

    firebase deploy

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div className='flex justify-center mt-4 md:mt-8 lg:mt-14'>
-            <img className='md:w-[50%] w-[90%]' src={`https://i.ibb.co/d2tpgQ7/404-error-page-not-found.png`} alt="" />
+            <img className='md:w-[50%] w-[90%]' src={`https://i.ibb.co/RPN8B3J/404-error-page-not-found-1.png`} alt="" />
         </div>
     );
 };
